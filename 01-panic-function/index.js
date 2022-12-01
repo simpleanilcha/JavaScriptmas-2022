3cc2354b5f3ff199235e04f87dbe57da76ebc0b8
Anil Maharjan <simpleanilcha@gmail.com>
2022-12-01 14:05:49 +0545

    solve: 01 day - panic function

---
+++ 01-panic-function/index.js
@@ -0,0 +1,32 @@
+/* Panic function 
+Write a PANIC! function. The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
+built in string methods. 
+
+If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+
+Example input: "Hello"
+Example output: "HELLO!"
+
+Example input: "I'm almost out of coffee"
+Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+*/
+
+function panic(str) {
+  let capStr = str.toUpperCase();
+  capStr += '!';
+  capStr = capStr.split(' ').join(' 😱 ')
+  return capStr;
+
+  // in short
+  // return str.toUpperCase().split(" ").join(" 😱 ") + "!"
+
+  // another method
+  // return `${str.replaceAll(' ', ' 😱 ').toUpperCase()}!`
+}
+
+// Test your function
+console.log(panic("I'm almost out of coffee")); 
+console.log(panic("winter is coming"));
+console.log(panic("Hello"));
+
